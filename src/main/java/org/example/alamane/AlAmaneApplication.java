843179498dd85d8a79371f88edc9287000950243
abdellatif guezadi <abdellatifguezadi@gmail.com>
2025-11-17 14:49:12 +0100

    first commit

---
+++ src/main/java/org/example/alamane/AlAmaneApplication.java
@@ -0,0 +1,13 @@
+package org.example.alamane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlAmaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlAmaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/alamane/AlAmaneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.alamane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlAmaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
